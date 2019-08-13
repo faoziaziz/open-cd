de600036b5cbbf6c2d5b4ea8b18b6d1f64826580
Aziz Amerul Faozi <admin@labseni.com>
2019-08-13 12:39:26 +0800

    video rendering complete

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,16 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv){
-  namedWIndow("Example3 ", WINDOW_AUTOSIZE);
-  cv::VideoCature cap;
-  cap.open(string argv[1]);
+  namedWindow("Example3 ", WINDOW_AUTOSIZE);
+  VideoCapture cap;
+  cap.open(string(argv[1]));
 
   Mat frame;
   for(;;){
     cap>>frame;
     if(frame.empty())break;
     imshow("exampler", frame);
-    if(waitkey(33)>=0)break;
+    if(waitKey(33)>=0)break;
   }
   return 0;
 }
